74b0adb82853018a13c8b8e7791764aa32eea87c
bertt <btemme@hotmail.com>
2014-01-24 00:04:36 +0100

    firstcommit

---
+++ KatanaHelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace KatanaHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var uri = "http://localhost:8080/";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+
+        }
+    }
+}
---
+++ KatanaHelloWorld/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace KatanaHelloWorld
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
