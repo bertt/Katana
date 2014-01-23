dfab99d4c94456f75654414e773b459b0c9767d1
bertt <btemme@hotmail.com>
2014-01-24 00:17:08 +0100

    more

--- KatanaHelloWorld/Startup.cs
+++ KatanaHelloWorld/Startup.cs
@@ -4,9 +4,20 @@ namespace KatanaHelloWorld
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration1(IAppBuilder app)
         {
             app.UseWelcomePage();
         }
+
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHandlerAsync((req, res) =>
+            {
+                res.ContentType = "text/plain";
+                return res.WriteAsync("Hello, World!");
+            });
+        }
+
     }
 }
