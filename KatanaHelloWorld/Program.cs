d2d94377c344baa2fb0e8a2b8a3e0237957c8d34
bertt <btemme@hotmail.com>
2014-01-24 11:31:50 +0100

    ddd

--- KatanaHelloWorld/Program.cs
+++ KatanaHelloWorld/Program.cs
@@ -4,27 +4,28 @@ using Microsoft.Owin.Hosting;
 
 namespace KatanaHelloWorld
 {
-	public class Program
-	{
-		private static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+    public class Program
+    {
+        private static ManualResetEvent _quitEvent = new ManualResetEvent(false);
 
-		static void Main(string[] args)
-		{
-			var port = 5000;
-			if (args.Length > 0)
-			{
-				int.TryParse(args[0], out port);
-			}
+        private static void Main(string[] args)
+        {
+            var port = 5000;
+            if (args.Length > 0)
+            {
+                int.TryParse(args[0], out port);
+            }
 
-			Console.CancelKeyPress += (sender, eArgs) =>
-			{
-				_quitEvent.Set();
-				eArgs.Cancel = true;
-			};
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
             using (WebApp.Start<Startup>(string.Format("http://*:{0}", port)))
-			{
-				Console.WriteLine("Started");
-				_quitEvent.WaitOne();
-			}
-		}
+            {
+                Console.WriteLine("Started");
+                _quitEvent.WaitOne();
+            }
+        }
+    }
 }
